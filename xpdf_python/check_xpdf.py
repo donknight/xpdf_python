063519fb5ae6a667975ebb43af054e52eed05dad
donknight <31926144+donknight@users.noreply.github.com>
2017-09-14 12:38:07 -0400

    Reformat pdftotext check
    Change the check to look a little saner.  If: pass else: dostuff hurts my brain a little.

--- xpdf_python/check_xpdf.py
+++ xpdf_python/check_xpdf.py
@@ -2,7 +2,5 @@ import os
 import sys
 
 # should cover windows, linux, or mac distros using either the pdftotext distro or the XPDF tools distro
-if any(i in os.environ['PATH'] for i in ('pdftotext', 'XPDF')):
-	pass
-else:
+if not any(i in os.environ['PATH'] for i in ('pdftotext', 'XPDF')):
 	sys.exit("Did not detect correctly installed xpdf. Please follow install instructions at: https://github.com/ecatkins/xpdf_python.")
