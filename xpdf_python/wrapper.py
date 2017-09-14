293a35fa8cdfcea13bc20f3380a313ff3b01baf5
donknight <31926144+donknight@users.noreply.github.com>
2017-09-14 10:45:26 -0400

    Implement page_num=False behavior
    Add behavior for not prepending pagenumbers to each page.
    
    Add options arg to allow passing options to pdftotext call

--- xpdf_python/wrapper.py
+++ xpdf_python/wrapper.py
@@ -13,13 +13,14 @@ def countPages(filename):
 	data = open(filename,"r", encoding = "ISO-8859-1").read()
 	return len(rxcountpages.findall(data))
 
-def to_text(file_loc, page_nums = True):
+def to_text(file_loc, page_nums=True, options=()):
 	''' Converts PDF to text
 
 	Args
 	- - - - - - -
 		file_loc: path to pdf document, string
 		page_nums: whether to insert page numbers into document, boolean
+		options: allows the addition of any of the normal options accepted by pdftotext, tuple of strings
 	
 	Returns
 	- - - - - - -
@@ -43,30 +44,37 @@ def to_text(file_loc, page_nums = True):
 
 	# If page numbers are to be inserted
 	if page_nums:
-		# Count number of pages
 		num = countPages(full_file_loc)
 		# Accounts for errors occuring in countPages function
 		if num == 0:
 			num = 100
-		for i in range(num):
-			actual = i + 1
-			# Calls xpdf 
-			subprocess.call(['pdftotext', '-f', str(actual),'-l', str(actual), full_file_loc])
-
-			# Opens file saved to disk, ensures it will always close when done
-			with open(saved_file, 'r', encoding='ISO-8859-1') as file:
-				t = file.read()
-			# If the page is blank, it is not a real page
-			if t == '':
-				continue
-			else:
-				actual_count += 1
-			# Add text and page count to existing string
-			text += '***Page {}*** {}'.format(actual, t)
-
 	else:
-		# TO BE IMPLEMENTED
-		pass
+		# accounts for not adding page numbers by allowing the loop to go just one bulk action
+		num = 1
+		actual_count = countPages(full_file_loc)  # try and provide a page estimate on bulk runs
+
+	for i in range(num):
+		actual = i + 1
+		opt = options
+		if page_nums:
+			opt += ('-f', str(actual), '-l', str(actual))
+
+		# Calls xpdf
+		subprocess.call(['pdftotext', *opt, full_file_loc])
+
+		# Opens file saved to disk, ensures it will always close when done
+		with open(saved_file, 'r', encoding='ISO-8859-1') as file:
+			t = file.read()
+		# If the page is blank, it is not a real page
+		if t == '':
+			continue
+		if page_nums:
+			actual_count += 1
+		# Add text and page count to existing string, or not it not page_nums
+		if page_nums:
+			text += '***Page {}*** {}'.format(actual, t)
+		else:
+			text = t
 
 	# Remove file saved to disk
 	os.remove(saved_file)
