92aeb817c4501f4787825fa861cbe2f264b290bc
donknight <31926144+donknight@users.noreply.github.com>
2017-09-14 09:50:05 -0400

    Make pdftotext check flexible
    Change pdftotext check from single path to environment PATH variable to better support for non-default installs.
    Add XPDF to path check to support Windows install of the XPDF port of pdftotext.

--- xpdf_python/check_xpdf.py
+++ xpdf_python/check_xpdf.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-if os.path.isfile('/usr/local/bin/pdftotext'):
+# should cover windows, linux, or mac distros using either the pdftotext distro or the XPDF tools distro
+if any(i in os.environ['PATH'] for i in ('pdftotext', 'XPDF')):
 	pass
 else:
 	sys.exit("Did not detect correctly installed xpdf. Please follow install instructions at: https://github.com/ecatkins/xpdf_python.")
